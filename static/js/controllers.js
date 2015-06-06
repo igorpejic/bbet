b643e507c5d46c661c812b7e75629067113c7575
matija077 <matija.prs@gmail.com>
2015-06-06 17:21:20 +0200

    change buttons

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -15,6 +15,8 @@ betControllers.controller('mainController', ['$scope', 'Song',
                    cellTemplate: '<div ng-click="add_bet(row.getProperty(col.field))" ng-bind="row.getProperty(col.field)"></div>'
                }
             ],
+            enableSorting:false,
+
         };
         $scope.BetGridOptions = {
             data: 'possible_bets',
@@ -23,18 +25,31 @@ betControllers.controller('mainController', ['$scope', 'Song',
                {
                    field: 'name',
                    displayName: 'Bets',
-                   cellTemplate: '<div style="display: inline-block;" ng-click="remove_bet(row.getProperty(col.field))" ng-bind="row.getProperty(col.field)"></div> <button class="btn btn-danger glyphicon glyphicon-trash delete-button"></button><button type="button" class="btn btn-primary bet-button" ">2</button><button class="btn btn-primary bet-button">X</button><button class="btn btn-primary bet-button">1</button>',
-                   enableRowSelection: true,
+                   cellTemplate: '<div   style="display: inline-block;" " ng-bind="row.getProperty(col.field)"></div> <button class="btn  btn-danger  glyphicon glyphicon-trash delete-button" ng-click="removeRow(row)" ></button><button type="button" class="btn btn-primary bet-button" ng-click="choose(row,2)">2</button><button class="btn btn-primary bet-button" ng-click="choose(row,0)">X</button><button class="btn btn-primary bet-button" ng-click="choose(row,1)">1</button>',
+                   
                }
-            ]
+            ],
+            enableSorting: false,
         };
         $scope.add_bet = function(name) {
-            $scope.possible_bets.push({'name': name});
+            $scope.possible_bets.push({'name': name, 'choice': ""});
             console.log($scope.possible_bets);
        
         };
-        $scope.remove_bet = function(name) {
-            var index = $scope.possible_bets.indexOf(name);
+
+        $scope.removeRow = function(row) {
+            var index = row.rowIndex;
+            $scope.BetGridOptions.selectItem(index, false);
             $scope.possible_bets.splice(index, 1);
         };
+
+        $scope.choose = function(row, choice) {
+            var index = row.rowIndex;
+            $scope.possible_bets[index].choice = choice;
+            alert($scope.possible_bets[index].choice); 
+        };
+
+            
+
+
     }]);
