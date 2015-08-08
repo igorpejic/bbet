251bf4ecc62b9d87923f03369868362ac44e674f
Igor Pejic <igor.pejic@dr.com>
2015-08-08 18:37:24 +0200

    improve betting ux

--- static/src/app/bet/singleBet.controller.js
+++ static/src/app/bet/singleBet.controller.js
@@ -17,22 +17,38 @@
 
         function addBet(newSong, choice){
 
-            // check if song is already present in bets and if present don't add it
-            for(var i=0;i<vm.bets.length;i++){
-                var temp = vm.bets[i];
-                if(angular.equals(temp.song, newSong)){
-                  return;
-                }
+            if (choice == '1') {
+                newSong.one = !newSong.one;
+            } else if(choice == '2') {
+                newSong.two = !newSong.two;
+            } else if (choice == 'x') {
+                newSong.x = !newSong.x;
             }
 
             var bet = {};
             bet.song = newSong;
             bet.choice = choice;
+            // check if song is already present in bets and if present remove it
+            for(var i=0;i<vm.bets.length;i++){
+                var temp = vm.bets[i];
+                if(angular.equals(temp, bet)){
+                    console.log('aa');
+                    vm.bets.splice(vm.bets.indexOf(temp), 1);
+                    return;
+                }
+            }
             vm.bets.push(bet);
             vm.hidden[newSong.position] = 1;
         }
 
         function removeBet(bet, index) {
+            if (bet.choice == '1') {
+                bet.song.one = !bet.song.one;
+            } else if(bet.choice == '2') {
+                bet.song.two = !bet.song.two;
+            } else if (bet.choice == 'x') {
+                bet.song.x = !bet.song.x;
+            }
             vm.hidden[bet.song.position] = 0;
             vm.bets.splice(index, 1);
         }
