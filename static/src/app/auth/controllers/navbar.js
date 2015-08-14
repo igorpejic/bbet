efd30fcfc1d9eb1a6b6dfbfc733e68fc6f64b583
Igor Pejic <igor.pejic@dr.com>
2015-08-14 21:24:41 +0200

    fix navbar login

--- static/src/app/auth/controllers/navbar.js
+++ static/src/app/auth/controllers/navbar.js
@@ -1,6 +1,23 @@
-angular.module('app.auth')
-  .controller('NavbarCtrl', function($scope, $auth, $resource) {
+(function() {
+angular
+    .module('app.auth')
+    .controller('NavbarCtrl', NavbarCtrl);
+
+NavbarCtrl.$inject = ['$scope', '$auth', '$resource'];
+
+function NavbarCtrl($scope, $auth, $resource) {
+    $scope.name = '';
+    $scope.bettingFunds = '';
+
     $scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
     };
-  });
+    var socialUser = $resource('/api/socialuser/', null, {'query': {method: 'GET', isArray:false}});
+    socialUser.query().$promise.then(
+        function success(data){
+            $scope.name = data.name;
+            $scope.bettingFunds = data.betting_funds;
+        }
+    );
+}
+})();
