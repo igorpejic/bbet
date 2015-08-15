03f7b6e39836f90d41047fa1b92b828c24337537
matija077 <matija.prs@gmail.com>
2015-08-15 17:06:02 +0200

    leaderboard skeleton in frontend

---
+++ static/src/app/leaderboard/leaderboard.controller.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+    angular
+        .module('app.leaderboard')
+        .controller('leaderboardController', leaderboardController);
+    
+    leaderboardController.$inject = [];
+
+    function leaderboardController() {
+        var vm = this;
+    }
+ 
+}());
