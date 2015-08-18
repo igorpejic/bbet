74956d92b6e383cb090ddf871d85c9dd818839ab
Igor Pejic <igor.pejic@dr.com>
2015-08-18 20:57:41 +0200

    fix tests

--- apps/bet/tests/test_api.py
+++ apps/bet/tests/test_api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
@@ -5,13 +7,15 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from model_mommy import mommy
 
-from apps.bet.models import Bet, Better
+from apps.bet.models import Bet, Better, Week
 
 
 class MyBetsViewTest(APITestCase):
 
     def setUp(self):
-        pass
+        today = datetime.date.today()
+        sunday = today + datetime.timedelta(days=-today.weekday() - 2, weeks=1)
+        mommy.make(Week, date=sunday)
 
     def test_dont_return_other_user_bets(self):
         self.user = mommy.make(User)
